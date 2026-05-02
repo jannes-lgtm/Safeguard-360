601b7a56925d841bfddeb27ae72454278d85bfd4
Jannes Keeve <janneskeeve@MacBookAir.lan>
2026-05-02 13:51:06 +0200

    Fine tune sidebar colour

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,7 @@ import {
 import { supabase } from '../lib/supabase'
 
 // Brand colours extracted from SafeGuard360 logo
-const BRAND_BLUE = '#1C1CFF'
+const BRAND_BLUE = '#1818FA'
 const BRAND_GREEN = '#AACC00'
 
 function NavSection({ label }) {
